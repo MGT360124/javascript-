dced700e27f4e9762602c10023d64a40aae716ce
maoguotao <1142656486@qq.com>
2018-08-24 10:41:19 +0800

    test文件夹

--- async/test/test.js
+++ async/test/test.js
@@ -30,3 +30,15 @@ gen().then(data => {
     console.log("err", err)  // gen()函数内部的错误，会被catch到
 });
 
+
+// async函数内部抛出错误，会导致返回的 Promise 对象变为reject状态。
+// 抛出的错误对象会被catch方法回调函数接收到。
+
+
+// async函数返回的 Promise 对象，必须等到内部所有await命令后面的 Promise 对象执行完，
+// 才会发生状态改变，除非遇到return语句或者抛出错误。
+// 也就是说，只有async函数内部的异步操作执行完，才会执行then方法指定的回调函数。
+
+// await
+// await命令后面是一个 Promise 对象。如果不是，会被转成一个立即resolve的 Promise 对象。
+// await命令后面的 Promise 对象如果变为reject状态，则reject的参数会被catch方法的回调函数接收到
